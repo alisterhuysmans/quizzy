a3ee4c59343eb75d330a78ca4ef9023b10bda8c4
Bouxadrien <adrien.boux@apprenant.ifapme.be>
2024-04-03 19:45:41 +0200

    réctification bouton rejouer

--- src/components/QuizResult.vue
+++ src/components/QuizResult.vue
@@ -2,7 +2,7 @@
     <div class="quiz-result">
       <h1>Résultats du Quiz</h1>
       <p>Votre score est : {{ score }} sur {{ totalPointsPossible }}</p>
-      <button @click="$emit('changePage', 'quizHome')">Rejouer</button>
+      <button @click="$emit('changePage', 'quizQuestion', currentCategoryId)">Rejouer</button>
       <button @click="$emit('changePage', 'quizHome')">Choisir une autre catégorie</button>
     </div>
   </template>
