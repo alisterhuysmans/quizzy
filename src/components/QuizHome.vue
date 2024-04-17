610e75698e5ff0925a3aae2cb8bee46416c4b7c7
Noah Depireux <depireuxnoah@gmail.com>
2024-04-17 22:09:31 +0200

    Update

--- src/components/QuizHome.vue
+++ src/components/QuizHome.vue
@@ -1,33 +1,56 @@
 <template>
-    <div class="quiz-home">
-      <h1>Choisissez une catégorie</h1>
+  <div class="quiz-home">
+    <h1 v-if="!errorMessage">Choisissez une catégorie</h1>
+    <div v-if="errorMessage" class="error-message">
+      <p>Il y as eu une erreur.</p>
+      {{ errorMessage }}</div>
+    <div v-else>
       <div v-for="category in categories" :key="category.id">
         <button @click="selectCategory(category.id)">{{ category.title }}</button>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        categories: [],
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [],
+      errorMessage: null,
+    };
+  },
+  mounted() {
+    this.fetchCategories();
+  },
+  methods: {
+    fetchCategories() {
+      fetch('https://quizz-musical-backend.airdev.be/api/categories')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Server Error');
+          }
+          return response.json();
+        })
+        .then(data => {
+          this.categories = data;
+          this.errorMessage = null;
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          this.errorMessage = 'An error occurred while fetching categories';
+        });
     },
-    mounted() {
-      this.fetchCategories();
-    },
-    methods: {
-      fetchCategories() {
-        fetch('https://quizz-musical-backend.airdev.be/api/categories')
-          .then(response => response.json())
-          .then(data => this.categories = data)
-          .catch(error => console.error('Error:', error));
-      },
-      selectCategory(id) {
-        this.$emit('changePage', 'quizQuestion', id);
-      }
+    selectCategory(id) {
+      this.$emit('changePage', 'quizQuestion', id);
     }
-  };
-  </script>
-  
+  }
+};
+</script>
+
+<style>
+.error-message {
+  color: red;
+  font-weight: bold;
+}
+</style>
